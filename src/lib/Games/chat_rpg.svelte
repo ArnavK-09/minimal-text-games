c04ea1e1040f6366156f14aeac46eb1a924c408d
Arnav K <arnavkaushal09@gmail.com>
2024-03-30 15:00:23 +0000

    add rpg prompt

--- src/lib/Games/chat_rpg.svelte
+++ src/lib/Games/chat_rpg.svelte
@@ -1,37 +1,52 @@
 <script lang="ts">
+	import Loader from '$lib/components/Loader.svelte';
 	import Button from '$lib/components/ui/button/button.svelte';
 	import Input from '$lib/components/ui/input/input.svelte';
-	import { test } from '$lib/gemini';
+	import { RPG_PROMPT, contactGemini } from '$lib/gemini';
 	import { randomNumber, updateUserScore } from '$lib/userScores';
+	import { marked } from 'marked';
+	import { onMount } from 'svelte';
 
 	type ChatEntry = {
 		from: 'bot' | 'player';
 		content: string;
 	};
 
-	let history: ChatEntry[] = [
-		{
-			from: 'bot',
-			content: '🎄 Hey rookie! Start your adventure now! Enter your command below....'
-		}
-	];
+	let history: Array<ChatEntry> = [];
 
 	let user_input: string = '';
-	let loading = false;
+	let loading = true;
 	export let data: any;
 	export let notHost: boolean = false;
 
-	function submit_user_entry() {
-		console.log(test());
-		if (user_input.trim().length == 0) return;
+	onMount(() => {
+		submit_user_entry(RPG_PROMPT, false);
+	});
+
+	async function submit_user_entry(
+		entry: string | MouseEvent = user_input,
+		addPlayerContent = true
+	) {
+		const message = entry.toString() as string;
+		if (message.trim().length == 0) return;
 		loading = true;
-		history = [
+		const response = await contactGemini(message);
+		const newhistory = [
 			...history,
+			await (async () => {
+				if (addPlayerContent)
+					return {
+						from: 'player',
+						content: await marked.parse(message.trim())
+					};
+				else return null;
+			})(),
 			{
-				from: 'player',
-				content: user_input.trim()
+				from: 'bot',
+				content: await marked.parse(response.trim())
 			}
 		];
+		history = newhistory.filter((x) => x !== null) as ChatEntry[];
 		loading = false;
 		user_input = '';
 		updateUserScore(randomNumber(150, 1));
@@ -41,6 +56,15 @@
 <svelte:head>
 	<title>Playing as {notHost ? 'Player' : 'Host'} | {data.games[data.game]}</title>
 </svelte:head>
+
+<svelte:document
+	on:keydown={(e) => {
+		if (e.key == 'Enter') {
+			submit_user_entry();
+		}
+	}}
+/>
+
 <section class="mt-16 grid w-screen place-items-center overflow-x-hidden">
 	<div
 		class="mx-auto block min-h-screen max-w-[70%] break-words rounded-lg text-center shadow-lg contrast-125"
@@ -51,16 +75,24 @@
 		<div
 			class="flex h-[50vh] max-h-[50vh] w-full max-w-[177%] flex-col overflow-y-scroll rounded-xl bg-white/5 py-4"
 		>
-			{#each history as chat}
+			{#each history.filter((x) => x !== null) as chat}
 				<div
-					class={`${chat.from == 'bot' ? '' : 'float-right brightness-110'} text-md w-[90% ] mx-4  my-2 break-words rounded-2xl bg-primary-foreground px-4 py-2 font-semibold`}
+					class={`${chat.from == 'bot' ? '' : 'float-right brightness-110'} text-md w-[90% ] prose prose-sm prose-invert mx-4 my-2 justify-center break-words rounded-2xl bg-primary-foreground px-4 py-2 font-semibold`}
 				>
-					{chat.content}
+					{@html chat.content}
 				</div>
 			{/each}
+			{#if history.length == 0}
+				<div class="grid h-full place-items-center opacity-85">
+					<Loader />
+				</div>
+			{/if}
 		</div>
 		<div>
 			<Input
+				autocomplete="false"
+				autocorrect="true"
+				spellcheck="false"
 				disabled={loading}
 				bind:value={user_input}
 				placeholder="Enter command..."
